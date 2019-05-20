0bcc99684f5a839740aa428fcdbdac2e0bca4a2c
busungg <busungg@naver.com>
2019-05-20 22:01:09 +0900

    v_0.0.2 (bug & feature)
    #14 [개선 사항] 등록/보기 모드 분리 plz
    1. Preview 버튼 추가
    2. Popup Window로 확인 가능한 Preview  화면 구성

--- src/js/html_builder.js
+++ src/js/html_builder.js
@@ -6,6 +6,7 @@
     var libObject = factory(root);
     root.HtmlBuilder.init = libObject.init;
     root.HtmlBuilder.getLayout = libObject.getLayout;
+    root.HtmlBuilder.showPreview = libObject.showPreview;
     
   } (typeof window !== 'undefined' ? window : this, function(win) {
       //HtmlBuilder
@@ -3128,6 +3129,37 @@
           for(var i = 0, len = O.css.length; i < len; i++) {
             defaultCss.appendChild(document.createTextNode(U.obj2Css(O.css[i]) + '\n\n'));
           }
+        };
+
+        H.showPreview = function() {
+          preview_window = window.open('', 'Preview', 'width=800,height=800');
+          
+          var head = document.getElementsByTagName('HEAD')[0];
+          var copy_head = head.cloneNode(true).children;
+          var preview_head = preview_window.document.getElementsByTagName('HEAD')[0];
+          var preview_body = preview_window.document.getElementsByTagName('BODY')[0];
+
+          var removeArray = [];
+          for(var i = 0, len = copy_head.length; i < len; i++) {
+            try{
+              if(copy_head[i].getAttribute('attr-type') === 'html_builder') {
+                removeArray.push(copy_head[i]);  
+              }
+            } catch(err){
+              removeArray.push(copy_head[i]);
+            }
+          }
+
+          while (removeArray.length > 0) {
+            removeArray[removeArray.length - 1].remove();
+            removeArray.splice(removeArray.length -1, 1);
+          }
+
+          while (copy_head.length > 0) {
+            preview_head.appendChild(copy_head[0]);
+          }
+
+          preview_body.innerHTML = U.exportHtml(H.config.ids[0]);
         };
         
       }(HtmlBuilder, Utils, Options));
