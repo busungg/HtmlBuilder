be5bfe2e91c714bff947d3ee045b0fcaf0d102c1
busungg <busungg@naver.com>
2020-03-01 17:01:11 +0900

    #41 [개선 사항] 프로그램 구조 리뉴얼
    1. 기존 Layout Class의 기능 리뉴얼 중
    1) Event 관리 기능 변경 중 - Layout class에서 단일로 사용 할 수 있는 방법을 찾아보는 중

--- src/js/layout_manager/model/newLayout.js
+++ src/js/layout_manager/model/newLayout.js
@@ -1,5 +1,10 @@
-//import Utils from '../../src/js/utils/utils';
+/*
+ * 1. 추후 layout.js를 대체
+ *  - 디자인 패턴을 변경한다.
+ * 2. ES6의 클래스를 사용하여 모듈 패턴을 구축한다.
+ */
 
+import Utils from '../../utils/utils';
 
 const Utils = {
     builder: function (option) {
@@ -48,12 +53,12 @@ const Utils = {
         }
     },
 
-    rgb2Hex: () => {
+    rgb2Hex: ()=> {
         return '#fff';
     }
 };
 
-class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
+class Layout {
 
     /*
         option
@@ -67,14 +72,6 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
         }
     */
     constructor(option) {
-        /*
-            option은 변경되면 안되기 때문에 closuer를 이용하여 private 접근자를 사용한다.
-        */
-        let _option = option;
-        this.getOption = () => {
-            return _option;
-        };
-
         this.dom = Utils.builder(option);
         //dom도 객체이기 때문에 layout property를 만들어서 자기 자신을 참조하게 한다.
         this.dom.layout = this;
@@ -85,95 +82,16 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
             width: 0,
             height: 0
         };
-        this.canHaveChild = option.canHaveChild;
-
-        /*
-            1. Dom마다 event 생성 이유
-                - Memory가 많이 든다 하더라고 어디에서 생성하더라도 단일로 관리 할 수 있도록 event를 각각 생성한다.
-            2. 주의 사항
-                - event combination이 생긴다.
-        */
-
-
-        /*
-            1. for select 표시할 때 사용
-         */
-        const mouseMove = (evt) => {
-            //새로운 element를 drag 중일 때
-            //또는 element를 click해서 옮기고 있을 시
-
-            this.dom;
-
-            evt.stopPropagation();
-        };
-
-        /*
-            1. for select
-        */
-        const click = (evt) => {
-            // => method의 this는 현재 scope로 정해지기 때문에 this를 사용할 수 있다.
-            this.dom;
-
-            evt.stopPropagation();
-        };
-
-        /*
-            1. for drag
-        */
-        const drag = (evt) => {
-            
-        };
-
-        /*
-             1. for drag end
-         */
-        const dragEnd = (evt) => {
-
-        };
-
-        /*
-            1. for drag over -> drag하는 대상이 존재 할때 mousemove 대신 사용
-        */
-       const dragOver = (evt) => {
-           //dataTransfer 사용
-       };
-
-       //dragleave
-       //drop
-
-       /*
-        function drag(ev) {
-        ev.dataTransfer.setData("text", ev.target.id);
-        }
-
-        function drop(ev) {
-        ev.preventDefault();
-        var data = ev.dataTransfer.getData("text");
-        
-        console.log(data);
-        
-        ev.target.appendChild(document.getElementById(data));
-        }
-       */
-
+        this.canHaveChild = option.isHave;
     }
 
-    //Layout을 꼭 미리 계산해야 하는가?
-    /*Contain은 과연 필요한가? 
-      그냥 이벤트 적용하면 되는거잖어 
-      bubbling만 피하면 되고
-      문제는 appendchild를 하면 안되고 순서를 정해야 한다. 왼쪽 오른쪽 밑에 위에 등 정해야 하는것이 있기때문에
-      계산해야하고 보여주기도 해야한다.
-    */
-
-    //필요한지 모르겠다.
     isContain(x, y) {
         try {
             if (!this.canHaveChild) {
                 return false;
             }
 
-            const pos = this.pos();
+            let pos = this.pos;
             if (pos.x <= x && x <= (pos.x + pos.width) &&
                 pos.y <= y && y <= (pos.y + pos.height)) {
                 return true;
@@ -185,25 +103,25 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
         }
     }
 
-    get pos() {
+    updateLayout() {
         try {
-            /*
-                1. 사용이유
-                    - pos는 필요할때마다 사용한다.
-                    - 변경이 있을 시 마다 pos가 update되어야 하기 때문인다.
-            */
-            const pos = {};
+            //This Layout
             const dom = this.dom
+            const pos = this.pos;
             const rect = dom.getBoundingClientRect() //render된 후의 top, left, width, height을 제공;
 
             //Offset의 기준이 되는 Parent Element
             const offsetParent = dom.offsetParent;
 
             /*
-                1. 주의 사항
-                    - OffsetLeft 등을 통해 offsetParent에서 얼마나 떨어져 있는지 알 수 있다.
-                    - padding이란 element 요소 안에 지정해둔 사이즈만큼 추가하는 것이다.
-                    - 30px * 30px element가 존재할 시 padding이 20px이면 width = 30 + 20 * 2, height = 30 + 20 * 2; 이다.
+                1. 시작 위치를 정확하게 파악해야한다.
+                2. 시작 위치에서 width와 height값을 통해 Element의 정확한 크기를 알아야 한다.
+                3. OffsetLeft 등을 통해 offsetParent에서 얼마나 떨어져 있는지 알 수 있다.
+
+                * padding을 헷갈리지 말자
+                * padding이란 element 요소 안에 지정해둔 사이즈만큼 추가하는 것이다.
+                * 30px * 30px element가 존재할 시 padding이 20px이면 width = 30 + 20 * 2, height = 30 + 20 * 2; 이다.
+
             */
             if (offsetParent) {
                 pos.x = dom.offsetLeft + (offsetParent.layout ? offsetParent.layout.pos.x : offsetParent.offsetLeft);
@@ -216,10 +134,7 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
             const isScrollX = (dom.scrollWidth - dom.clientWidth);
             const isScrollY = (dom.scrollHeight - dom.clientHeight);
 
-            /*
-                1. 사용이유
-                    - getBoundingClientRect를 사용하는 이유 -> inline 요소의 width, height값을 확인하기 위함
-            */
+            //getBoundingClientRect를 사용하는 이유 -> inline 요소의 width, height값을 확인하기 위함
             pos.width = (isScrollX ? dom.scrollWidth : rect.width);
             pos.height = (isScrollY ? dom.scrollHeight : rect.height);
 
@@ -361,11 +276,11 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
                         property.style[key] = groupProperty[key].value;
                     }
 
-                    /*group화 할 시 사용
+                    /*group 화 할 시 사용
                     groupName = key.split('-');
                     for (i = 0, len = direction.length; i < len; i++) {
                         propertyName = ((groupName.length > 1) ? (groupName[0] + direction[i] + '-' + groupName[1]) : (groupName[0] + direction[i]));
-                        property.style[propertyName] = null;
+                        prop.style[propertyName] = null;
                     }
                     */
                 }
@@ -376,67 +291,4 @@ class Layout { //단일 Dom처럼 사용하기 위해 생성한 Class
             console.log(err.message);
         }
     }
-
-    copy() {
-        function copyRecursive(parentDom) {
-            const option = parentDom.layout.getOption();
-            const copiedLayout = new Layout(option);
-            const children = [...parentDom.children];
-
-            for (let child of children) {
-                copiedLayout.dom.appendChild(copyRecursive(child));
-            }
-
-            return copiedLayout.dom;
-        }
-
-        const copiedLayoutDom = copyRecursive(this.dom);
-        this.dom.parentNode.appendChild(copiedLayoutDom);
-    };
-
-    delete() {
-        function deleteRecursive(parentDom) {
-            /*
-                1. 사용이유
-                 -  순환참조로 인하여 dom이 삭제되도 layout에서 참조되어 메모리에서
-                    삭제되지 않을까봐 layout부터 null로 처리한다.
-            */
-            const children = [...parentDom.children];
-
-            for (let child of children) {
-                deleteRecursive(child);
-            }
-
-            parentDom.layout = null;
-        }
-
-        const dom = this.dom;
-        const parent = this.dom.parentNode;
-        deleteRecursive(dom);
-        parent.removeChild(this.dom);
-    }
 }
-
-
-const LayoutDiv1 = new Layout({
-    element: 'div',
-    attr: {
-        style: 'width:100px; height:100px; padding:20px; margin:20px; border: 1px solid #dddddd'
-    }
-});
-
-const LayoutDiv2 = new Layout({
-    element: 'div',
-    attr: {
-        style: 'width:30px; height:30px; padding:20px; margin:20px; border: 1px solid #dddddd'
-    }
-});
-
-const LayoutSpan = new Layout({
-    element: 'span',
-    text: 'text'
-});
-
-document.body.appendChild(LayoutDiv1.dom);
-LayoutDiv1.dom.appendChild(LayoutDiv2.dom);
-LayoutDiv2.dom.appendChild(LayoutSpan.dom);
