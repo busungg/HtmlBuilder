9a85c6b3ebdebaa26b818ac377149cebcc98793d
busungg <busungg@naver.com>
2022-07-20 23:39:05 +0900

    Update Component
    1. added value attribute
    2. added name attribute

--- src/components/model/Component.js
+++ src/components/model/Component.js
@@ -5,4 +5,22 @@ export default class Component extends Model {
   constructor() {
     super();
   }
+
+  get value() {
+    return this.get('value');
+  }
+
+  set value(_value) {
+    this.set('value', _value);
+  }
+
+  get name() {
+    return this.get('name');
+  }
+
+  set name(value) {
+    this.set('name', value);
+  }
+
+  get classList() {}
 }
--- test/components/model/Component.js
+++ test/components/model/Component.js
@@ -8,31 +8,56 @@ describe('Component', () => {
   let component;
   before(() => {
     component = new Component();
-    component.set('value', '1');
-    component.set('name', 'component');
+    component.value = '1';
+    component.name = 'component';
+    component.id = 'c1';
   });
 
   describe('attributes', () => {
-    it('should return attributes { value: 1, name: "component"}', () => {
-      const expectResult = {
-        value: 1,
-        name: 'component'
-      };
+    describe('properties', () => {
+      it('should return attributes { value: 1, name: "component"}', () => {
+        const expectResult = {
+          id: 'c1',
+          value: 1,
+          name: 'component'
+        };
 
-      Object.entries(component.attributes).forEach(([key, value]) => {
-        console.log(key, value);
-        assert.equal(value, expectResult[key]);
+        Object.entries(component.attributes).forEach(([key, value]) => {
+          assert.equal(value, expectResult[key]);
+        });
+      });
+
+      it('should return value "1"', () => {
+        assert.equal(component.value, '1');
+      });
+
+      it('should return name "component"', () => {
+        assert.equal(component.name, 'component');
+      });
+
+      it('should return id "c1"', () => {
+        assert.equal(component.id, 'c1');
       });
     });
 
-    it('should return value "1"', () => {
-      console.log(component);
+    describe('methods', () => {
+      it('should set properties "{id: c2, value: 1, name: c2}"', () => {
+        component.set({
+          id: 'c2',
+          value: 1,
+          name: 'c2'
+        });
 
-      assert.equal(component.value, '1');
-    });
+        const expectResult = {
+          id: 'c2',
+          value: 1,
+          name: 'c2'
+        };
 
-    it('should return name "component"', () => {
-      assert.equal(component.name, 'component');
+        Object.entries(component.attributes).forEach(([key, value]) => {
+          assert.equal(value, expectResult[key]);
+        });
+      });
     });
   });
 
@@ -40,7 +65,7 @@ describe('Component', () => {
     describe('properties', () => {
       it('should return length 1', () => {});
 
-      if (('should return value "test1"', function () {}));
+      it(('should return value "test1"', function () {}));
     });
 
     describe('methods', () => {
